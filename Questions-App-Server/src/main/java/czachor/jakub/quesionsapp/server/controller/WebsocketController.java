791f8887659d910dca819fa538303234bf7fc99d
Jakub Czachor <jakubczachor7@gmail.com>
2019-06-20 22:11:22 +0200

    Add backend (#3)
    * DTO changes.
    
    * Add mapper.
    
    * Add backend.

---
+++ Questions-App-Server/src/main/java/czachor/jakub/quesionsapp/server/controller/WebsocketController.java
@@ -0,0 +1,55 @@
+package czachor.jakub.quesionsapp.server.controller;
+
+import czachor.jakub.quesionsapp.server.models.dto.QuestionDTO;
+import czachor.jakub.quesionsapp.server.models.dto.QuestionLookupDTO;
+import czachor.jakub.quesionsapp.server.models.message.QuestionsMessage;
+import czachor.jakub.quesionsapp.server.service.QuestionService;
+import czachor.jakub.quesionsapp.server.util.Mapper;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class WebsocketController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final QuestionService questionService;
+
+    public WebsocketController(SimpMessagingTemplate simpMessagingTemplate, QuestionService questionService) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.questionService = questionService;
+    }
+
+    @MessageMapping("/questions")
+    private void message(QuestionsMessage message) {
+        System.out.println("Received message: " + message);
+        switch (message.getType()) {
+            case ALL:
+                this.simpMessagingTemplate.convertAndSend("/questions/", this.questionLookupDTO());
+                break;
+            case SINGLE:
+                QuestionDTO singleDto = Mapper.map(questionService.getById(message.getMessageId()));
+                this.simpMessagingTemplate.convertAndSend("/questions/" + message.getMessageId(), singleDto);
+                break;
+            case UNLOCK:
+                this.questionService.unlockById(message.getMessageId());
+                this.simpMessagingTemplate.convertAndSend("/questions/", this.questionLookupDTO());
+                break;
+            case ANSWER:
+                this.questionService.addAnswer(message.getMessageId(), message.getAnswers());
+                QuestionDTO answered = Mapper.map(questionService.getById(message.getMessageId()));
+                this.simpMessagingTemplate.convertAndSend("/questions/" + message.getMessageId(), answered);
+                break;
+            case RESET:
+                this.questionService.resetAll();
+                this.simpMessagingTemplate.convertAndSend("/questions/", this.questionLookupDTO());
+                break;
+        }
+    }
+
+    private List<QuestionLookupDTO> questionLookupDTO() {
+        return Mapper.map(this.questionService.getAll());
+    }
+
+}
---
+++ Questions-App-Server/src/main/java/czachor/jakub/quesionsapp/server/service/QuestionService.java
@@ -0,0 +1,40 @@
+package czachor.jakub.quesionsapp.server.service;
+
+import czachor.jakub.quesionsapp.server.models.AnswerHolder;
+import czachor.jakub.quesionsapp.server.models.QuestionState;
+import czachor.jakub.quesionsapp.server.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public List<AnswerHolder> getAll() {
+        return questionRepository.getAll();
+    }
+
+    public AnswerHolder getById(Long id) {
+        return questionRepository.getById(id).orElse(null);
+    }
+
+    public void unlockById(Long id) {
+        questionRepository.getById(id).orElse(null).setState(QuestionState.ACTIVE);
+    }
+
+    public void addAnswer(Long messageId, List<Long> answers) {
+        AnswerHolder holder = getById(messageId);
+        answers.sort(Comparator.comparingLong(Long::longValue));
+        holder.getAnswered().merge(answers, 1L, Long::sum);
+    }
+
+    public void resetAll() {
+        this.questionRepository.reset();
+    }
+}
