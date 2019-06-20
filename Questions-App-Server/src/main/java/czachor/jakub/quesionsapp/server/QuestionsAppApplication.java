4c7d92c7a505057e911b3b02762a561410847165
jakubczachor7@gmail.com <Pantyry123>
2019-06-20 19:23:01 +0200

    init

---
+++ Questions-App-Server/src/main/java/czachor/jakub/quesionsapp/server/QuestionsAppApplication.java
@@ -0,0 +1,11 @@
+package czachor.jakub.quesionsapp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionsAppApplication.class, args);
+    }
+}
