7b2ee0bb5f028ad289083e37609cbfc41713b2bc
jakubczachor7@gmail.com <Pantyry123>
2019-06-23 11:32:49 +0200

    add websocket client.

--- Questions-App-Client/app/src/main/java/czachor/jakub/questions/app/AnswersApplication.java
+++ Questions-App-Client/app/src/main/java/czachor/jakub/questions/app/AnswersApplication.java
@@ -8,11 +8,16 @@ import czachor.jakub.questions.app.models.sqlite.DaoMaster;
 import czachor.jakub.questions.app.models.sqlite.DaoSession;
 import czachor.jakub.questions.app.models.sqlite.User;
 import czachor.jakub.questions.app.models.sqlite.UserDao;
+import czachor.jakub.questions.app.utils.Subscriptions;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
 
 public class AnswersApplication extends Application {
     private static AnswersApplication application;
     private DaoSession daoSession;
     private User user;
+    private StompClient stompClient;
+    private Subscriptions subscriptions;
 
     @Override
     public void onCreate() {
@@ -20,12 +25,19 @@ public class AnswersApplication extends Application {
         application = this;
         this.initDatabase();
         this.loadUser();
+        this.initWebsocket();
     }
 
     private void initDatabase() {
         DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "answers-db");
         Database db = helper.getWritableDb();
         daoSession = new DaoMaster(db).newSession();
+    }
+
+    public void initWebsocket() {
+        stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://10.0.2.2:8080/ws/websocket");
+        stompClient.connect();
+        subscriptions = new Subscriptions();
     }
 
     private void loadUser() {
@@ -44,5 +56,13 @@ public class AnswersApplication extends Application {
 
     public String role() {
         return user.getRole();
+    }
+
+    public StompClient getStompClient() {
+        return stompClient;
+    }
+
+    public Subscriptions getSubscriptions() {
+        return subscriptions;
     }
 }
