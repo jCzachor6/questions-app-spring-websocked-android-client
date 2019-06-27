d13feac9d347fc8568432b7e35abb0b83e340894
jakubczachor7@gmail.com <Pantyry123>
2019-06-27 22:22:30 +0200

    Answers holding in database.

--- Questions-App-Client/app/src/main/java/czachor/jakub/questions/app/models/sqlite/Answer.java
+++ Questions-App-Client/app/src/main/java/czachor/jakub/questions/app/models/sqlite/Answer.java
@@ -13,31 +13,62 @@ public class Answer {
     private Long questionId;
     @NotNull
     private String answer;
-    @Generated(hash = 1403800245)
-    public Answer(Long id, @NotNull Long questionId, @NotNull String answer) {
+    @NotNull
+    private String correctAnswer;
+    @NotNull
+    private String state;
+
+    @Generated(hash = 55578741)
+    public Answer(Long id, @NotNull Long questionId, @NotNull String answer,
+            @NotNull String correctAnswer, @NotNull String state) {
         this.id = id;
         this.questionId = questionId;
         this.answer = answer;
+        this.correctAnswer = correctAnswer;
+        this.state = state;
     }
+
     @Generated(hash = 53889439)
     public Answer() {
     }
+
     public Long getId() {
         return this.id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public Long getQuestionId() {
         return this.questionId;
     }
+
     public void setQuestionId(Long questionId) {
         this.questionId = questionId;
     }
+
     public String getAnswer() {
         return this.answer;
     }
+
     public void setAnswer(String answer) {
         this.answer = answer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
--- Questions-App-Client/app/src/main/java/czachor/jakub/questions/app/utils/Mapper.java
+++ Questions-App-Client/app/src/main/java/czachor/jakub/questions/app/utils/Mapper.java
@@ -2,14 +2,17 @@ package czachor.jakub.questions.app.utils;
 
 import czachor.jakub.questions.app.models.AnswerDto;
 import czachor.jakub.questions.app.models.sqlite.Answer;
+import czachor.jakub.questions.app.models.sqlite.AnswerState;
 
 public class Mapper {
     public static AnswerDto map(Answer answer) {
         AnswerDto dto = new AnswerDto();
-        if(answer != null) {
+        if (answer != null) {
             dto.setAnswer(answer.getAnswer());
             dto.setId(answer.getId());
             dto.setQuestionId(answer.getQuestionId());
+            dto.setCorrectAnswer(answer.getCorrectAnswer());
+            dto.setState(AnswerState.fromName(answer.getState()));
         }
         return dto;
     }
